ef879231e479f2c32ac2f542699e8060baddc572
WJoey-Barton <wjoey.barton@gmail.com>
2026-04-11 22:20:29 -0400

    Fix equals method implementation in GameResult

--- src/GameResult.java
+++ src/GameResult.java
@@ -48,5 +48,6 @@ public class GameResult {
     public boolean equals(Object obj) {
         if(this == obj) return true;
         if(obj == null || getClass() != obj.getClass()) return false;
+        return false;
     }
 }
